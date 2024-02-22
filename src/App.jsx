70940e31b4d9213c6054bce8395659fe480123cb
Cordero Leonel Rodrigo <leonelrcordero22@gmail.com>
2024-02-22 20:40:19 -0300

    Se cambiaron los estilos de las cards y los botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Inicio from "./components/pages/Inicio"
+import Inicio from './components/pages/Inicio';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
 
 function App() {
   return (
     <>
-      <Inicio/>
+      <Inicio />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/CardReceta.jsx
+++ src/CardReceta.jsx
@@ -7,22 +7,32 @@ import {
   CardText,
   CardTitle,
 } from 'react-bootstrap';
+import './style/cardReceta.css';
 
 const CardReceta = ({ recetas }) => {
   return (
-    <div className='col-lg-6'>
-      <Card>
-        <CardImg src='https://images.pexels.com/photos/2641886/pexels-photo-2641886.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' />
-        <CardBody>
-          <CardTitle>{recetas.nombreReceta}</CardTitle>
-          <CardText>
+    <div className='col-lg-6 p-3'>
+      <div className='contenedor'>
+        <div className='imgContenedor'>
+          <img
+            src='https://images.pexels.com/photos/2641886/pexels-photo-2641886.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            alt='Receta'
+          />
+        </div>
+        <div className='my-3'>
+          <h3 className='m-0'>{recetas.nombreReceta}</h3>
+        </div>
+        <div>
+          <p className='m-0 my-3'>
             {recetas.fecha} | por {recetas.autor}
-            <br></br>
+            <br />
             {recetas.descripcionBreve}
-          </CardText>
-          <Button variant='primary'>Seguir leyendo</Button>
-        </CardBody>
-      </Card>
+          </p>
+          <button className='p-2'>
+            Seguir leyendo <i class='bi bi-arrow-right'></i>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
