42233a77d99814d7be060b272add664ea9631c60
brunomry <brunomadozzo@gmail.com>
2024-02-22 17:42:45 -0300

    Agregue footer en componente Footer con logo y grilla de 2 columnas de informacion y redes sociales con iconos de bootstrap. Ademas agregue copyright de derechos reservados.

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
-
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer className="text-center bgFooter text-light pt-5">
+      <section className="mb-5">
+        <article className="text-center">
+          <img src="" alt="logo" width={150} />
+        </article>
+        <article className="my-5 row justify-content-center gap-4">
+          <div className="col-md-6 col-lg-4">
+            <h5 className="mb-2">Información</h5>
+            <p className="d-flex justify-content-center gap-2 align-items-center">
+              <i class="bi bi-google text-danger fs-2"></i>
+              <span>blogrecetas@gmail.com</span>
+            </p>
+          </div>
+          <div className="col-md-6 col-lg-4">
+            <h5 className="mb-2">Seguinos en redes sociales</h5>
+            <div className="d-flex gap-3 justify-content-center">
+              <a href="">
+                <i class="bi bi-facebook fs-2"></i>
+              </a>
+              <a href="">
+                <i class="bi bi-instagram fs-2 text-danger"></i>
+              </a>
+            </div>
+          </div>
+        </article>
+      </section>
+      <p>&copy;Blog Todos los derechos reservados. 2024</p>
+    </footer>
   );
 };
 
