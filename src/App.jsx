a2e008a6e30f8ed17229febb96ae5dd5535e22ef
brunomry <brunomadozzo@gmail.com>
2024-02-22 16:48:53 -0300

    Agregue las opciones del menu con navbar de react-bootstrap con sus rutas en componente Menu y tambien agregue las rutas de paginas en app.jsx mediante componentes de react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import './App.css';
+import Footer from './components/common/Footer';
+import Menu from './components/common/Menu';
+import Administrador from './components/pages/Administrador';
+import Inicio from './components/pages/Inicio';
+import Error404 from './components/pages/Error404';
+import Nosotros from './components/pages/Nosotros';
+import DetalleReceta from './components/pages/receta/DetalleReceta';
+import FormularioReceta from './components/pages/receta/FormularioReceta';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+      <Menu></Menu>
+      <Routes>
+        <Route exact path="/" element={<Inicio></Inicio>}></Route>
+        <Route path="/administrador" element={<Administrador></Administrador>}></Route>
+        <Route path="/nosotros" element={<Nosotros></Nosotros>}></Route>
+        <Route path="/detalleReceta" element={<DetalleReceta></DetalleReceta>}></Route>
+        <Route path="/administrador/crear" element={<FormularioReceta></FormularioReceta>}></Route>
+        <Route path="/administrador/editar/:id" element={<FormularioReceta></FormularioReceta>}></Route>
+        <Route path="*" element={<Error404></Error404>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </BrowserRouter>
   )
 }
 
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import { Nav, Navbar, Container } from "react-bootstrap";
+import {Link, NavLink} from "react-router-dom";
 
 const Menu = () => {
   return (
-    <div>
-      
-    </div>
+    <header>
+      <Navbar expand="md" className="bg-body-tertiary">
+        <Container className="d-flex flex-md-column gap-3">
+          <Navbar.Brand as={Link} to="/" className="mt-3">
+            <img src="" alt="logo" className="logo" width={150} />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+          >
+            <Nav className="ms-auto text-center">
+              <NavLink end className="nav-link me-3 fs-5" to="/">Inicio</NavLink>
+              <NavLink end className="nav-link me-3 fs-5" to="/recetas">Recetas</NavLink>
+              <NavLink end className="nav-link me-3 fs-5" to="/administrador">Administrador</NavLink>
+              <NavLink end className="nav-link me-3 fs-5" to="/nosotros">Nosotros</NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 };
 
