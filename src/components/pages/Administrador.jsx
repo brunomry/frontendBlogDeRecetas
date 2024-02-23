1e3cd2bf461a1124eb543475c3b4da73ecb9d471
Javier Jimenez <javier.sniguel@gmail.com>
2024-02-23 16:24:07 -0300

    Se creo la tabla con las columnas de las recetas

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,11 +1,39 @@
 import { Button, Table } from "react-bootstrap";
-import ItemProducto from "./receta/ItemReceta";
+import ItemReceta from "./receta/ItemReceta";
 
 const Administrador = () => {
   return (
-    <div>
-      <h1>hola mundo</h1>
-    </div>
+       <section className="container mainSection">
+      <div className="d-flex justify-content-between align-items-center mt-5">
+        <h1 className="display-4 ">Lista de Recetas registradas</h1>
+        <Button className="btn btn-primary" >
+          <i className="bi bi-file-earmark-plus"></i>
+        </Button>
+      </div>
+      <hr />
+      <Table responsive striped bordered hover>
+        <thead>
+          <tr className="text-center">
+            <th>Id</th>
+            <th>Nombre de Receta</th>
+            <th>Ingredientes</th>
+            <th>Preparación</th>
+            <th>Fecha</th>
+            <th>Imagen</th>
+            <th>Desc Breve</th>
+            <th>Desc Amplia</th>
+            <th>Autor</th>
+            <th>Opciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          <ItemReceta></ItemReceta>
+          <ItemReceta></ItemReceta>
+          <ItemReceta></ItemReceta>
+          <ItemReceta></ItemReceta>
+        </tbody>
+      </Table>
+    </section>
   );
 };
 
