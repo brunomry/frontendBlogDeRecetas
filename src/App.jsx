6c64a919d03708a979701bdd061a7804dbb91553
brunomry <brunomadozzo@gmail.com>
2024-02-23 04:41:43 -0300

    Agregue una seccion en componente DetalleReceta con titulo, imagen, fecha, autor, descripcion de receta, lista de ingredientes y lista de preparacion aplicando clases de bootstrap y estilos css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import DetalleReceta from './components/pages/receta/DetalleReceta';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
     <>
-    
+      <DetalleReceta></DetalleReceta>
     </>
   )
 }
