4407e11fc9e61231aa7063f4bc6f4fe9f738c3cd
Javier Jimenez <javier.sniguel@gmail.com>
2024-02-23 22:59:16 -0300

    se colocaron los import de booststrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+import "./App.css";
+import Error404 from "./components/pages/Error404";
 function App() {
   return (
     <>
