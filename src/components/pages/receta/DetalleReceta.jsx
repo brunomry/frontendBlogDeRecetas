ea712b00e31e0ff52d465b12769cdc45bb445a29
brunomry <brunomadozzo@gmail.com>
2024-02-27 16:50:32 -0300

    Conversion de string a array usando split de las propiedades ingredientes y preparacion para mostrar informacion mediante lista

--- src/components/pages/receta/DetalleReceta.jsx
+++ src/components/pages/receta/DetalleReceta.jsx
@@ -24,43 +24,41 @@ const DetalleReceta = () => {
     }
   };
 
-  return (
-    !receta ? (
-      <Error404 />
-    ) : (
-      <section className="container py-5">
-        <article className="detailRecipe mx-auto mb-5">
-          <h1 className="text-center mb-4">{receta.nombreReceta}</h1>
-          <figure className="text-center">
-            <img
-              src={imagenReceta}
-              alt="imagen de receta"
-              className="imgReceta"
-            />
-            <figcaption className="mt-2">
-              {receta.fecha} | Por {receta.autor}
-            </figcaption>
-          </figure>
-          <p>{receta.descripcionAmplia}</p>
-          <h3>Ingredientes</h3>
-          <ul>
-            {receta.ingredientes.map((ingrediente, index) => (
-              <li key={index}>{ingrediente}</li>
-            ))}
-          </ul>
-          <h3>Preparación</h3>
-          <ol type="1">
-            {receta.preparacion.map((paso, index) => (
-              <li key={index}>{paso}</li>
-            ))}
-          </ol>
-        </article>
-        <article className="detailRecipe mx-auto">
-          <FormComentarioReceta />
-        </article>
-      </section>
-    )
-  );  
-}
+  return !receta ? (
+    <Error404 />
+  ) : (
+    <section className="container py-5">
+      <article className="detailRecipe mx-auto mb-5">
+        <h1 className="text-center mb-4">{receta.nombreReceta}</h1>
+        <figure className="text-center">
+          <img
+            src={imagenReceta}
+            alt="imagen de receta"
+            className="imgReceta"
+          />
+          <figcaption className="mt-2">
+            {receta.fecha} | Por {receta.autor}
+          </figcaption>
+        </figure>
+        <p>{receta.descripcionAmplia}</p>
+        <h3>Ingredientes</h3>
+        <ul>
+          {receta.ingredientes.split(",").map((ingrediente, index) => (
+            <li key={index}>{ingrediente}</li>
+          ))}
+        </ul>
+        <h3>Preparación</h3>
+        <ol type="1">
+          {receta.preparacion.split(".").map((paso, index) => (
+            <li key={index}>{paso}</li>
+          ))}
+        </ol>
+      </article>
+      <article className="detailRecipe mx-auto">
+        <FormComentarioReceta />
+      </article>
+    </section>
+  );
+};
 
 export default DetalleReceta;
