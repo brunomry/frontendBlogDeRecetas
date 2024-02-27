b9a08ddc302fba3f47af8e8d6e72f225584fed81
brunomry <brunomadozzo@gmail.com>
2024-02-27 17:38:29 -0300

    correccion de logica

--- src/components/pages/receta/DetalleReceta.jsx
+++ src/components/pages/receta/DetalleReceta.jsx
@@ -24,40 +24,40 @@ const DetalleReceta = () => {
     }
   };
 
-  return !receta ? (
-    <Error404 />
-  ) : (
+  return receta ? (  
     <section className="container py-5">
-      <article className="detailRecipe mx-auto mb-5">
-        <h1 className="text-center mb-4">{receta.nombreReceta}</h1>
-        <figure className="text-center">
-          <img
-            src={imagenReceta}
-            alt="imagen de receta"
-            className="imgReceta"
-          />
-          <figcaption className="mt-2">
-            {receta.fecha} | Por {receta.autor}
-          </figcaption>
-        </figure>
-        <p>{receta.descripcionAmplia}</p>
-        <h3>Ingredientes</h3>
-        <ul>
-          {receta.ingredientes.split(",").map((ingrediente, index) => (
-            <li key={index}>{ingrediente}</li>
-          ))}
-        </ul>
-        <h3>Preparación</h3>
-        <ol type="1">
-          {receta.preparacion.split(".").map((paso, index) => (
-            <li key={index}>{paso}</li>
-          ))}
-        </ol>
-      </article>
-      <article className="detailRecipe mx-auto">
-        <FormComentarioReceta />
-      </article>
-    </section>
+    <article className="detailRecipe mx-auto mb-5">
+      <h1 className="text-center mb-4">{receta.nombreReceta}</h1>
+      <figure className="text-center">
+        <img
+          src={imagenReceta}
+          alt="imagen de receta"
+          className="imgReceta"
+        />
+        <figcaption className="mt-2">
+          {receta.fecha} | Por {receta.autor}
+        </figcaption>
+      </figure>
+      <p>{receta.descripcionAmplia}</p>
+      <h3>Ingredientes</h3>
+      <ul>
+        {receta.ingredientes.split(",").map((ingrediente, index) => (
+          <li key={index}>{ingrediente}</li>
+        ))}
+      </ul>
+      <h3>Preparación</h3>
+      <ol type="1">
+        {receta.preparacion.split(".").map((paso, index) => (
+          <li key={index}>{paso}</li>
+        ))}
+      </ol>
+    </article>
+    <article className="detailRecipe mx-auto">
+      <FormComentarioReceta />
+    </article>
+  </section>
+  ) : (
+    <Error404 />
   );
 };
 
